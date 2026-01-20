5b018d60631fe8d99390bccef103cb9aa9f8d00c
Vladyslav Diachenko <vladislav2569@gmail.com>
2026-01-20 18:29:39 +0100

    update Debug

--- DjangoGramm/settings.py
+++ DjangoGramm/settings.py
@@ -21,7 +21,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY CONFIGURATION
 SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = env('DEBUG')
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 
